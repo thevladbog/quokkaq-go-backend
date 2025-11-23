fb239b1dd487be43f256958c5b4e8fca7a1df094
Vladislav Bogatyrev <vladislav.bogatyrev@gmail.com>
2025-11-23 15:46:12 +0300

    feat: Implement `asynq` job client and worker for TTS generation.

--- internal/jobs/client.go
+++ internal/jobs/client.go
@@ -32,6 +32,7 @@ func NewJobClient() JobClient {
 	}
 
 	redisAddr := fmt.Sprintf("%s:%s", redisHost, redisPort)
+	fmt.Printf("DEBUG: Redis Client Config: Host='%s', Port='%s', Addr='%s'\n", redisHost, redisPort, redisAddr)
 
 	client := asynq.NewClient(asynq.RedisClientOpt{
 		Addr:     redisAddr,
