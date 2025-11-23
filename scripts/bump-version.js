8f3e3e5986b8fc497a16cc6cd650ae7c88e89668
Vladislav Bogatyrev <vladislav.bogatyrev@gmail.com>
2025-11-23 14:46:12 +0300

    feat: Add GitHub Actions workflow for automated production release with version bumping and Yandex Cloud deployment.

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -5,7 +5,19 @@ const versionFilePath = path.join(__dirname, '../VERSION');
 const currentVersion = fs.readFileSync(versionFilePath, 'utf8').trim();
 
 const versionParts = currentVersion.split('.').map(Number);
-versionParts[2] += 1;
+const type = process.argv[2] || 'patch';
+
+if (type === 'major') {
+    versionParts[0] += 1;
+    versionParts[1] = 0;
+    versionParts[2] = 0;
+} else if (type === 'minor') {
+    versionParts[1] += 1;
+    versionParts[2] = 0;
+} else {
+    versionParts[2] += 1;
+}
+
 const newVersion = versionParts.join('.');
 
 fs.writeFileSync(versionFilePath, newVersion);
